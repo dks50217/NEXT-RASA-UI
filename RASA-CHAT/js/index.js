e147fe7f97130cc1d0190dd1b967a15a20dbc21b
Michael_Chang <Michael_Chang@pegatroncorp.com>
2022-06-15 18:33:48 +0800

    add demo and docker-compose.yml

---
+++ RASA-CHAT/js/index.js
@@ -0,0 +1,65 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
+const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
+const BOT_NAME = "BOT";
+const PERSON_NAME = "Michael";
+//const requestURL = "http://localhost:5005/webhooks/rest/webhook";
+const requestURL = "http://172.18.203.113:5005/webhooks/rest/webhook";
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+  var dataJSON = {};
+  dataJSON["sender"] = PERSON_NAME;
+  dataJSON["message"] = msgText;
+
+  botResponse(dataJSON);
+});
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function botResponse(dataJSON) {
+  axios({method: 'POST',url: requestURL, data: JSON.stringify(dataJSON)})
+  .then(function (response) {appendMessage(BOT_NAME, BOT_IMG, "left", response.data[0].text)})
+  .catch(function (error) {console.log(error)})
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
